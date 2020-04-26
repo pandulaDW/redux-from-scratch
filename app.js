fcfc2bd7f27beb98ccd8d301702eaa16c48a1fa7
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2020-04-26 12:14:31 +0530

    Added business logic functions

--- app.js
+++ app.js
@@ -5,13 +5,24 @@ const { countActions, toDoActions } = require('./actions');
 // Creating the store
 const store = createStore(rootReducer);
 
+// Subscribing a listener
+store.subscribe(() => console.log(store.getState()));
+
+// Creating the business logic
+const addTodoLogic = (todo) => {
+  store.dispatch(toDoActions.addTodo(todo));
+  store.dispatch(countActions.incrementCount());
+};
+
+const removeTodoLogic = (todo) => {
+  store.dispatch(toDoActions.removeTodo(todo));
+  store.dispatch(countActions.decrementCount());
+};
+
 // Dispatch an action
-store.dispatch(toDoActions.addTodo('brush teeth'));
-store.dispatch(toDoActions.addTodo('wash face'));
-store.dispatch(toDoActions.removeTodo('brush teeth'));
-store.dispatch(countActions.incrementCount());
-store.dispatch(countActions.incrementCount());
-store.dispatch(countActions.decrementCount());
+addTodoLogic('brush teeth');
+addTodoLogic('wash face');
+removeTodoLogic('brush teeth');
 
 // Unsubscribing a listener
 store.unsubscribe();
